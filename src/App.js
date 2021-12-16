6aac53dc5bfc5b2733820e4207290ea1639f988c
kikluz <savir81@hotmail.com>
2021-12-16 12:00:26 -0800

    add new project slider

--- src/App.js
+++ src/App.js
@@ -8,6 +8,67 @@ function App() {
   // states values 
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+  // alternative 
+  // useEffect(() => {
+  //   // gets the last item in the array 
+  //   const lastIndex = people.length - 1;
+  //   // if index less then 0 setIndex to the last one 
+  //   if (index < 0) {
+  //     setIndex(lastIndex)
+  //   }
+  //   if (index > lastIndex) {
+  //     setIndex(0);
+  //   }
+  // }, [index, people])
+
+  // this way is not using the the useEffect, here in the fucntion will check for the index
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      // here we increasing the lide 
+      let index = oldIndex + 1
+      if (index > people.length - 1) {
+        index = 0;
+      }
+      return (
+        index
+      )
+    })
+  }
+
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      // here we increasing the lide 
+      let index = oldIndex - 1
+      if (index < 0) {
+        index = people.length - 1;
+      }
+      return (
+        index
+      )
+    })
+  }
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1
+        if (index > people.length - 1) {
+          index = 0
+        }
+        return index
+      })
+    }, 5000)
+    return () => clearInterval(slider)
+  }, [index])
+
+
+  // for using the useEffect 
+  // useEffect(() => {
+  //   let slider = setInterval(() => {
+  //     setIndex(index + 1)
+  //   }, 5000)
+  //   return () => clearInterval(slider)
+  // }, [index])
 
 
   return (
@@ -23,8 +84,16 @@ function App() {
         {people.map((person, personIndex) => {
           const { id, name, image, title, quote } = person;
 
+          let position = 'nextSlide';
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+            position = 'lastSlide'
+          }
+
           return (
-            <article key={id}>
+            <article key={id} className={position}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
@@ -34,10 +103,14 @@ function App() {
           )
 
         })}
-        <button className="prev" onClick={() => { }}>
+        {/* for useEffect   */}
+        {/* <button className="prev" onClick={() => setIndex(index - 1)}> */}
+        <button className="prev" onClick={prevSlide}>
           <FiChevronLeft />
         </button>
-        <button className="next" onClick={() => { }}>
+        {/* for useEffect   */}
+        {/* <button className="next" onClick={() => setIndex(index + 1)}> */}
+        <button className="next" onClick={nextSlide}>
           <FiChevronRight />
         </button>
       </div>
